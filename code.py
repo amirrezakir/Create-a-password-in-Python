43f5c97f130f934850f93a1f9cacba74470aa05c
Amirreza karimi <86169058+amirrezakarimip@users.noreply.github.com>
2021-11-16 10:35:44 +0330

    Create code.py

---
+++ code.py
@@ -0,0 +1,26 @@
+import string
+import random
+ 
+if __name__ == "__main__":
+ str1 = string.ascii_lowercase
+ # print(s1)
+ str2 = string.ascii_uppercase
+ # print(s2)
+ str3 = string.digits
+ # print(s3)
+ str4 = string.punctuation
+ # print(s4)
+ pwdlen = int(input("Enter password length\n"))
+ s = []
+ s.extend(list(str1))
+ s.extend(list(str2))
+ s.extend(list(str3))
+ s.extend(list(str4))
+ # print(s)
+ # random.shuffle(s)
+ # print(s)
+ print("Your password is: ")
+ print("".join(random.sample(s, pwdlen)))
+ # print("".join(s[0:pwdlen]))
+ 
+a = input("")
